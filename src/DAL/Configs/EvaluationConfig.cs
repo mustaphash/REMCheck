1981c3bea0d45d14b2af227f9960ca3721274ba0
Mustafa Shakir <mshakir9918@gmail.com>
2024-02-26 23:48:08 +0200

    Create all configs

---
+++ src/DAL/Configs/EvaluationConfig.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configs
+{
+    public class EvaluationConfig : IEntityTypeConfiguration<Evaluation>
+    {
+        public void Configure(EntityTypeBuilder<Evaluation> builder)
+        {
+
+            builder.Property(x => x.Precent)
+                .IsRequired();
+
+            builder.Property(x => x.Description)
+                .HasMaxLength(250)
+                .IsRequired();
+        }
+    }
+}
---
+++ src/DAL/Configs/ReportConfig.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configs
+{
+    public class ReportConfig : IEntityTypeConfiguration<Report>
+    {
+        public void Configure(EntityTypeBuilder<Report> builder)
+        {
+            builder.Property(r => r.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(r => r.Description)
+                .HasMaxLength(250)
+                .IsRequired();
+        }
+    }
+}
