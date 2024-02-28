b1effeb9ed319cf6b134b64ce025a850353402b2
Mustafa Shakir <mshakir9918@gmail.com>
2024-02-28 16:01:00 +0200

    Add EmployeeResponseModel

---
+++ src/REMCheck/Models/EmployeeModels/EmployeeResponseModel.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+
+namespace REMCheck.Models.EmployeeModels
+{
+    public class EmployeeResponseModel
+    {
+        public EmployeeResponseModel(Employee employee)
+        {
+            Id = employee.Id;
+            FirstName = employee.FirstName;
+            LastName = employee.LastName;
+            CheckedDate = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime CheckedDate { get; set; }
+    }
+}
