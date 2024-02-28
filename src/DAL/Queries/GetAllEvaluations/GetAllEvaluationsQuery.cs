a6dc71118ce8a5650078f463160650653d469bcf
Mustafa Shakir <mshakir9918@gmail.com>
2024-02-28 15:43:06 +0200

    Create Queries for Evaluations

---
+++ src/DAL/Queries/GetAllEvaluations/GetAllEvaluationsQuery.cs
@@ -0,0 +1,9 @@
+﻿using Core.Entities;
+using Core.Queries;
+
+namespace DAL.Queries.GetAllEvaluations
+{
+    public class GetAllEvaluationsQuery : IQuery<IList<Evaluation>>
+    {
+    }
+}
---
+++ src/DAL/Queries/GetAllEvaluations/GetAllEvaluationsQueryHandle.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Core.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Queries.GetAllEvaluations
+{
+    public class GetAllEvaluationsQueryHandle : IQueryHandler<GetAllEvaluationsQuery, IList<Evaluation>>
+    {
+        private readonly REMContext _rEMContext;
+
+        public GetAllEvaluationsQueryHandle(REMContext rEMContext)
+        {
+            _rEMContext = rEMContext;
+        }
+
+        public async Task<IList<Evaluation>> HandleAsync(GetAllEvaluationsQuery query, CancellationToken cancellationToken = default)
+        {
+            List<Evaluation> evaluations = await _rEMContext.Evaluations.ToListAsync(cancellationToken);
+
+            return evaluations;
+        }
+    }
+}
