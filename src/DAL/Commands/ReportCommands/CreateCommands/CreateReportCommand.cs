2d1de482170d3f06ef44cb40407332d76a593e55
Mustafa Shakir <mshakir9918@gmail.com>
2024-02-29 14:34:03 +0200

    Add CreateCommand to Reports

---
+++ src/DAL/Commands/ReportCommands/CreateCommands/CreateReportCommand.cs
@@ -0,0 +1,14 @@
+﻿using Core.Commands;
+using Core.Entities;
+
+namespace DAL.Commands.ReportCommands.CreateCommands
+{
+    public class CreateReportCommand : ICommand
+    {
+        public CreateReportCommand(Report report)
+        {
+            Report = report;
+        }
+        public Report Report { get; }
+    }
+}
---
+++ src/DAL/Commands/ReportCommands/CreateCommands/CreateReportCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Core.Commands;
+
+namespace DAL.Commands.ReportCommands.CreateCommands
+{
+    public class CreateReportCommandHandler : ICommandHandler<CreateReportCommand>
+    {
+        private readonly REMContext _rEMContext;
+
+        public CreateReportCommandHandler(REMContext rEMContext)
+        {
+            _rEMContext = rEMContext;
+        }
+
+        public async Task HandleAsync(CreateReportCommand command, CancellationToken cancellationToken = default)
+        {
+            await _rEMContext.AddAsync(command.Report);
+            await _rEMContext.SaveChangesAsync();
+        }
+    }
+}
